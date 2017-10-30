c6c30fb0cb5070a52811d18654cc9a27583a45bc
Lucas Tavares <lucastavaresdarocha@gmail.com>
2017-10-30 08:51:04 -0200

    cria html cadastro

--- alurabooks/src/App.js
+++ alurabooks/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import $ from 'jquery'
 import logo from './logo.svg'
 import './css/pure.css'
 import './css/side-menu.css'
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = { autores: [] }
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: 'http://localhost:8080/api/autores',
+      dataType: 'json',
+      success: res => this.setState({ autores: res })
+    })
+  }
+
   render() {
     return (
       <div id="layout">
-        <a href="#menu" id="menuLink" class="menu-link">
+        <a href="#menu" id="menuLink" className="menu-link">
           <span></span>
         </a>
         <div id="menu">
@@ -15,9 +30,12 @@ class App extends Component {
             <a className="pure-menu-heading" href="#">Company</a>
 
             <ul className="pure-menu-list">
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>
-
+              <li className="pure-menu-item">
+                <a href="#" className="pure-menu-link">Home</a>
+              </li>
+              <li className="pure-menu-item">
+                <a href="#" className="pure-menu-link">About</a>
+              </li>
               <li className="pure-menu-item menu-item-divided pure-menu-selected">
                 <a href="#" className="pure-menu-link">Services</a>
               </li>
@@ -28,24 +46,50 @@ class App extends Component {
         </div>
         <div id="main">
           <div className="header">
-            <h1>Page Title</h1>
-            <h2>A subtitle for your page goes here</h2>
+            <h1>Cadastro de Autores</h1>
           </div>
           <div className="content">
-            <h2 className="content-subhead">How to use this layout</h2>
-            <p>
-              To use this layout, you can just copy paste the HTML, along with the CSS in <a href="/css/layouts/side-menu.css" alt="Side Menu CSS">side-menu.css</a>, and the JavaScript in <a href="/js/ui.js">ui.js</a>. The JS file uses vanilla JavaScript to simply toggle an <code>active</code> className that makes the menu responsive.
-            </p>
+            <div className="pure-form pure-form-aligned">
+              <form className="pure-form pure-form-aligned">
+                <div className="pure-control-group">
+                  <label htmlFor="nome">Nome</label>
+                  <input id="nome" type="text" name="nome" value=""  />
+                </div>
+                <div className="pure-control-group">
+                  <label htmlFor="email">Email</label>
+                  <input id="email" type="email" name="email" value=""  />
+                </div>
+                <div className="pure-control-group">
+                  <label htmlFor="senha">Senha</label>
+                  <input id="senha" type="password" name="senha"  />
+                </div>
+                <div className="pure-control-group">
+                  <label></label>
+                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                </div>
+              </form>
+            </div>
 
-            <h2 className="content-subhead">Now Let's Speak Some Latin</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-
-            <h2 className="content-subhead">Try Resizing your Browser</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <div>
+              <table className="pure-table">
+                <thead>
+                  <tr>
+                    <th>Nome</th>
+                    <th>email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    this.state.autores.map(autor => 
+                      <tr key={ autor.id }>
+                        <td>{ autor.nome }</td>
+                        <td>{ autor.email }</td>
+                      </tr>
+                    )
+                  }
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
